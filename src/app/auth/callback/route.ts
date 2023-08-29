2d18026b528168c1d822abb3678ef6fe655b886d
Jair Diaz <jairdiaz@blacksip.com>
2023-08-28 22:59:16 -0500

    feat: routes

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -8,8 +8,7 @@ export async function GET(request: NextRequest) {
   const requestURL = new URL(request?.url || '');
   const code = requestURL.searchParams.get('code');
   if (code != null) {
-    const cookieStore = cookies();
-    const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+    const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
   return NextResponse.redirect(requestURL.origin);
