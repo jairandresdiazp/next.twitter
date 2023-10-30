b7500fbb2638397e13e53e1bce5120d4db897ac7
Jair Diaz <jairdiaz@blacksip.com>
2023-10-30 13:47:18 -0500

    feat: debug

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -7,6 +7,7 @@ export const dynamic = 'force-dynamic';
 export async function GET(request: NextRequest) {
   const requestURL = new URL(request?.url || '');
   const code = requestURL.searchParams.get('code');
+  console.debug('request login', JSON.stringify(requestURL));
   if (code != null) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
